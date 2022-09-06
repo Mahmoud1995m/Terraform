afb6e6d4b3cefe7a4eaef2cbde059a785fdde245
Mahmoud1995m <mabufarha11@gmail.com>
2022-09-06 23:48:54 +0300

    all

--- terraform_s3/variables.tf
+++ terraform_s3/variables.tf
@@ -3,6 +3,6 @@ variable "external_port" {
   default = 80
   validation {
     condition     = can(regex("8080|80", var.external_port))
-    error_message = "Port values can only be 8080 or 80!!"
+    error_message = "Port values can only be 8080 or 80!!!!!!"
   }
 }
